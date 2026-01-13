cdcb5b481a95ba31f00903c27a64e3b206f96edb
Adrien DA COSTA VEIGA <adrien.dacostaveiga@jetdev.fr>
2026-01-13 11:32:31 +0100

    trying things 3

--- tina/config.ts
+++ tina/config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "tinacms";
 
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
 export default defineConfig({
-  branch: "main",
+  branch,
+
+  // Get this from tina.io
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
   token: process.env.TINA_TOKEN,
 
   build: {
@@ -21,7 +31,7 @@ export default defineConfig({
       {
         name: "post",
         label: "Posts",
-        path: "src/content/posts",
+        path: "content/posts",
         fields: [
           {
             type: "string",
